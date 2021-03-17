df3b831d2c91816f69de639017dcbb1b4341e416
lauralandi <80513459+lauralandi@users.noreply.github.com>
2021-03-17 12:57:10 +0100

    Update R_code_remote_sensing_first.r

--- R_code_remote_sensing_first.r
+++ R_code_remote_sensing_first.r
@@ -7,3 +7,10 @@ setwd("C:/lab/")
 # p224r63_2011 <- brick("p224r63_2011_masked.grd")
 # p224r63_2011
 # plot(p224r63_2011)
+
+#cambio colore
+cl1<-colorRampPalette(c("black","grey","light grey")) (100)
+#plot(p224r63_2011,col=cl1)
+
+cl2<-colorRampPalette(c("blue","green","yellow","pink")) (100)
+#plot(p224r63_2011,col=cl1)
