64b456581ceac3869631c082c546bef4c3bc2a89
lauralandi <80513459+lauralandi@users.noreply.github.com>
2021-03-24 13:13:10 +0100

    Update R_code_remote_sensing_first.r

--- R_code_remote_sensing_first.r
+++ R_code_remote_sensing_first.r
@@ -1,16 +1,53 @@
 # Primo codice in R per telerilevamento
 # install.packages("raster")
-library(raster)
+library(raster) #richiamo il pacchetto raster
 
-setwd("C:/lab/")
+setwd("C:/lab/") #imposto la working directory
 
-# p224r63_2011 <- brick("p224r63_2011_masked.grd")
-# p224r63_2011
-# plot(p224r63_2011)
+### BANDE LANDSAT:
+#B1= blu
+#B2= verde
+#B3= rosso
+#B4= near infrared
+#B5= middle infrared
+#B6= infrarosso termico
+#B7= infrarosso medio (altro sensore)
 
-#cambio colore
-cl1<-colorRampPalette(c("black","grey","light grey")) (100)
-#plot(p224r63_2011,col=cl1)
+p224r63_2011 <- brick("p224r63_2011_masked.grd") # con la funzione brick estraggo i dati raster e li associo alla variabile
+p224r63_2011 #leggo le informazioni raster
+plot(p224r63_2011) #plotto l'immagine con la scala colori di default
 
-cl2<-colorRampPalette(c("blue","green","yellow","pink")) (100)
-#plot(p224r63_2011,col=cl1)
+cl1<-colorRampPalette(c("blue","green","orange","yellow")) (100) #definisco una nuova scala colore
+plot(p224r63_2011,col=cl1) #plotto l'immagine con la nuova scala colore
+
+dev.off() #pulisco la finestra grafica
+plot(p224r63_2011$B1_sre, col=cl1) #plotto una sola banda a scelta usando $
+
+#con la funzione par definisco la visualizzazione dei grafici in righe e colonne a mia scelta:
+par(mfrow=c(2,1)) # 2 righe e 1 colonna
+plot(p224r63_2011$B1_sre,col=cl1)
+plot(p224r63_2011$B2_sre,col=cl1)
+
+par(mfrow=c(4,1)) # 4 righe e 1 colonna
+plot(p224r63_2011$B1_sre,col=cl1)
+plot(p224r63_2011$B2_sre,col=cl1)
+plot(p224r63_2011$B3_sre,col=cl1)
+plot(p224r63_2011$B4_sre,col=cl1)
+
+par(mfrow=c(2,2)) # 2 righe e 2 colonne
+plot(p224r63_2011$B1_sre,col=cl1)
+plot(p224r63_2011$B2_sre,col=cl1)
+plot(p224r63_2011$B3_sre,col=cl1)
+plot(p224r63_2011$B4_sre,col=cl1)
+
+#associo una scala colore diversa ad ogni banda e le plotto in un quadrato 2x2
+par(mfrow=c(2,2))
+clb<- colorRampPalette(c("darkblue", "blue", "lightblue")) (100)
+clv<- colorRampPalette(c("darkgreen", "green", "lightgreen")) (100)
+clr<- colorRampPalette(c("darkred", "red", "orange")) (100)
+clnir<- colorRampPalette(c("pink", "salmon","magenta")) (100)
+plot(p224r63_2011$B1_sre,col=clb)
+plot(p224r63_2011$B2_sre,col=clv)
+plot(p224r63_2011$B3_sre,col=clr)
+plot(p224r63_2011$B4_sre,col=clnir)
+
