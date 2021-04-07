527968ee5a830d99a72a00fa0ec7217f88f218d2
lauralandi <80513459+lauralandi@users.noreply.github.com>
2021-04-07 13:09:57 +0200

    Update R_code_time_series.r

--- R_code_time_series.r
+++ R_code_time_series.r
@@ -6,3 +6,18 @@
 library(raster)
 
 setwd("C:/lab/greenland/")
+lst_2000<-raster("lst_2000.tif") #importo un layer raster
+lst_2005<-raster("lst_2005.tif")
+lst_2010<-raster("lst_2010.tif")
+lst_2015<-raster("lst_2015.tif")
+
+#creare multipanel con le 4 immagini
+par(mfrow=c(2,2))
+plot(lst_2000)
+plot(lst_2005)
+plot(lst_2010)
+plot(lst_2015)
+
+rlist<-list.files(pattern="lst") #creo una lista di file e associo alla variabile rlist
+import<-lapply(rlist,raster) #applico la funzione raster su tutti i file della lista e associo alla variabile import (in questo modo li importo tutti insieme)
+TGr<-stack(import) # creo un univo file che contiene tutti quelli della lista importata
