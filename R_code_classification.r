6bde8e501ea3d9999189096f95b34c035f5796e3
lauralandi <80513459+lauralandi@users.noreply.github.com>
2021-04-21 12:06:40 +0200

    Update R_code_classification.r

--- R_code_classification.r
+++ R_code_classification.r
@@ -3,9 +3,18 @@
 ### DATI SOLAR ORBITER
 
 library(raster)
-library(ncdf4)
+
 library(RStoolbox) #richiamo il pacchetto RStoolbox
 library(ggplot2) #richiamo il pacchetto ggplot2
-library(rasterVis)
+
 
 setwd("C:/lab/")
+
+so <- brick("Solar_Orbiter_s_first_views_of_the_Sun_pillars.jpg")
+so
+
+plotRGB(so, 1,2,3, stretch="lin") #visualizzo le tre bande montate in RGB
+
+set.seed(42) # fa in modo che il risultato sia sempre lo stesso nei diversi run utilizzando sempre lo stesso set di pixel che altrimenti sarebbe sempre randomica
+so_c <- unsuperClass(so, nClasses=3) #classificazone non supervisionata
+so_c20 <- unsuperClass(so, nClasses=20) #classificazione con 20 classi
