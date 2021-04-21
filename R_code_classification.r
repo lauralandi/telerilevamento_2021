a31b28bf3a9547b4cbf2a3eb1267846e65278479
lauralandi <80513459+lauralandi@users.noreply.github.com>
2021-04-21 12:10:11 +0200

    Update R_code_classification.r

--- R_code_classification.r
+++ R_code_classification.r
@@ -11,10 +11,11 @@ library(ggplot2) #richiamo il pacchetto ggplot2
 setwd("C:/lab/")
 
 so <- brick("Solar_Orbiter_s_first_views_of_the_Sun_pillars.jpg")
-so
-
+so #vedo le info della immagine
+plot(so) #visualizzo le tre bande separate
 plotRGB(so, 1,2,3, stretch="lin") #visualizzo le tre bande montate in RGB
 
 set.seed(42) # fa in modo che il risultato sia sempre lo stesso nei diversi run utilizzando sempre lo stesso set di pixel che altrimenti sarebbe sempre randomica
-so_c <- unsuperClass(so, nClasses=3) #classificazone non supervisionata
+so_c <- unsuperClass(so, nClasses=3) #classificazone non supervisionata che produce una mappa e il model con le info
 so_c20 <- unsuperClass(so, nClasses=20) #classificazione con 20 classi
+plot(so_c$map) # plotto la mappa prodotta dalla funzione unsuperClass
