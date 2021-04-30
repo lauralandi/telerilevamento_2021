763d03fd44bb6c39fcf6bfc0e8479d9d18fe342e
lauralandi <80513459+lauralandi@users.noreply.github.com>
2021-04-30 10:07:11 +0200

    Update R_code_vegetation_indices.r

--- R_code_vegetation_indices.r
+++ R_code_vegetation_indices.r
@@ -12,3 +12,29 @@ defor2<-brick("defor2.jpg")
 par(mfrow=c(2,1))
 plotRGB(defor1, r=1, g=2, b=3, stretch='lin')
 plotRGB(defor2, r=1, g=2, b=3, stretch='lin')
+
+#le bande dentro defor1 si chiamano defor1.1, defor 1.2, defor1.3
+cl <- colorRampPalette(c('darkblue','yellow','red','black'))(100) # specifying a color scheme
+DVI1<-defor1$defor1.1 - defor1$defor1.2 #calcolo DVI sottraendo B1-B2 (NIR-RED)
+plot(DVI1, col=cl, main="DVI at time 1")
+
+DVI2<-defor2$defor2.1 - defor2$defor2.2 #calcolo DVI sottraendo B1-B2 (NIR-RED)
+plot(DVI2, col=cl, main="DVI at time 2")
+
+par(mfrow=c(2,1))
+plot(DVI1, col=cl, main="DVI at time 1")
+plot(DVI2, col=cl, main="DVI at time 2")
+
+#sottraiamo il secondo DVI al primo per capire di quanto è calato nel tempo
+deltaDVI<- DVI1 - DVI2
+cld <- colorRampPalette(c('blue','white','red'))(100) 
+plot(deltaDVI, col=cld, main="differenza DVI")
+
+## calcolo del NDVI
+## NDVI= (NIR-RED)/(NIR+RED)
+
+NDVI1<-(defor1$defor1.1-defor1$defor1.2)/(defor1$defor1.1+defor1$defor1.2)
+NDVI2<-(defor2$defor2.1-defor2$defor2.2)/(defor2$defor2.1+defor2$defor2.2)
+par(mfrow=c(2,1))
+plot(NDVI1, col=cl, main="NDVI at time 1")
+plot(NDVI2, col=cl, main="NDVI at time 2")
