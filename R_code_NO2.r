5c9a507e34ecb0d2878ab046de8e185884b4a8ba
lauralandi <80513459+lauralandi@users.noreply.github.com>
2021-06-14 10:30:52 +0200

    Create R_code_NO2.r

---
+++ R_code_NO2.r
@@ -0,0 +1,15 @@
+# R_code_NO2.r
+
+
+
+library(raster) 
+library(RStoolbox) 
+library(ggplot2) 
+library(rasterVis)
+library(knitr)
+library(ncdf4)
+library(gridExtra)
+library(viridis)
+
+
+setwd("C:/lab/")
