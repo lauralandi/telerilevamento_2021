b4b2edca3d5151a4ac745d474c8cd51dc7ef2884
lauralandi <80513459+lauralandi@users.noreply.github.com>
2021-08-06 11:27:41 +0200

    Update R_code_copernicus.r

--- R_code_copernicus.r
+++ R_code_copernicus.r
@@ -1,21 +1,24 @@
 ### VISUALIZZAZIONE DATI COPERNICUS
 
+# con installl.packages installo i pacchetti che mi interessano (è sufficiente farlo una volta)
+# una volta installati i pacchetti uso library per richiamare quelli che servono nel codice e poterli utilizzare
+
 #install.packages("ncdf4")
-library(raster)
-library(ncdf4)
-library(RStoolbox) #richiamo il pacchetto RStoolbox
-library(ggplot2) #richiamo il pacchetto ggplot2
-library(rasterVis)
+library(raster) # pacchetto con funzioni per elaborare file raster
+library(ncdf4)  # pacchetto che permette di importare ed elaborare file in formato .nc
+library(RStoolbox) # pacchetto con funzioni per processare le immagini (tra cui unsuperClass)
+library(ggplot2) # pacchetto con diverse funzioni per creare e modificare grafici
+library(rasterVis) # pacchetto con funzioni aggiuntive per elaborare oggetti raster
 
-setwd("C:/lab/")
+setwd("C:/lab/")  # definisco la working directory
 
-SWI<-raster("c_gls_SWI1km_202104071200_CEURO_SCATSAR_V1.0.1.nc")
+SWI<-raster("c_gls_SWI1km_202104071200_CEURO_SCATSAR_V1.0.1.nc")  # importo il file come oggetto raster
 
 cl <- colorRampPalette(c("red","pink","light blue","blue"))(100)
 cl2 <- colorRampPalette(c("red","yellow","pink","white"))(100)
-#plot(SWI,col=cl)
-levelplot(SWI,col.regions=cl2)
+levelplot(SWI,col.regions=cl2)  # plotto l'immagine con la scala colori scelta
 
 ## resampling
-SWIres<-aggregate(SWI,fact=10)
-plot(SWIres,col=cl,main="Soil Water Index")
+SWIres<-aggregate(SWI,fact=10) # con la funzione aggregate posso ricampionare il raster, ovvero ridurre il numero di pixel
+                               # con fact=10 calcola per ogni gruppo 10x10 di pixel una media producendo per ognuna di queste un solo pixel più grande in uscita
+plot(SWIres,col=cl,main="Soil Water Index") # plotto il risultato del ricampionamento
