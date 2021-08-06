f607fba4bfe6c465f7dfe06420f9f7dc756d8947
lauralandi <80513459+lauralandi@users.noreply.github.com>
2021-08-06 11:28:12 +0200

    Update R_code_knitr.r

--- R_code_knitr.r
+++ R_code_knitr.r
@@ -1,6 +1,9 @@
 ### REPORT IN KNITR
 
-setwd("C:/lab/")
-library(knitr)
-stitch("R_code_greenland.r", template=system.file("misc", "knitr-template.Rnw", package="knitr"))
+setwd("C:/lab/") # definisco la working directory
+
+library(knitr)  # il pacchetto knitr permette di realizzare dei report
+
+stitch("R_code_greenland.r", template=system.file("misc", "knitr-template.Rnw", package="knitr")) # la funzione stitch crea un report automatico sulla base di uno
+                                                                                                  # script R, salvato nella wd e definito dal suo path, e un template  
 
